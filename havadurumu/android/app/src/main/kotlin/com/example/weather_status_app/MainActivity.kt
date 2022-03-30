e1966f8cd104d533bcd52d14c709e557aae5ae24
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2022-03-30 09:10:23 +0300

    Delete havadurumu directory

--- havadurumu/android/app/src/main/kotlin/com/example/weather_status_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weather_status_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- havadurumu/lib/designfunctions/shadows.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Shadow textShadow() {
-  return Shadow(blurRadius: 30, color: Colors.red, offset: Offset(-2, 2));
-}
